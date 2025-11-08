837a116f278be9a674185cf9be2eaf337b4fc56f
vlad <vlad.kiaune@gmail.com>
2025-11-08 14:35:18 +0300

    Initial Commit

---
+++ gisty/gistyApp.swift
@@ -0,0 +1,17 @@
+//
+//  gistyApp.swift
+//  gisty
+//
+//  Created by admin on 08.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct gistyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
